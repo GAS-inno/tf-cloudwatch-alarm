43f728c3f4c0b169a445c131cafb7efb0b749c1e
SawGweAw <saw.gweaw@gmail.com>
2025-10-05 03:51:41 +0800

    update name

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 data "aws_caller_identity" "current" {}
 
 locals {
-  name_prefix = "Saw1"#split("/", "${data.aws_caller_identity.current.arn}")[1]
+  name_prefix = "Saw" #split("/", "${data.aws_caller_identity.current.arn}")[1]
 }
